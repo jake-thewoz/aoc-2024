bf3be67229c132486a4d9191ba74e17d8e17eae7
Jakob <jake.thewoz@gmail.com>
2024-12-13 17:14:37 +0100

    Tried again, still failed

--- 6.py
+++ 6.py
@@ -58,80 +58,92 @@ print('The total distinct positions travelled was', total_Xs)
 
 # part two ----------------------------------------------------------------------------------------
 
-# wow.
-# I think I've got it! Blocking opportunities come from two situations:
-#   - when you cross a path you've been on 
-#   - when the next non '.' tile to your right is either a turn or a line in the direction you're looking!
-# This requires me to rewrite part one, so I'll reset variables and go again down here.
 
+# Part Two ----- Fresh Start
+
+# every turn, we need to do the following
+#   - look at the next square
+#       - if the next square is #, we need to turn
+#           - we check again, because we may need to turn twice
+#       - if the next square is ., we need to check for a loop
+#           - we start by saving the start position
+#           - maybe we make a copy of the map, with one new obstacle in our way 
+#           - then we follow the rules:
+            #   - look at the next square
+            #       - if the next square is #, we need to turn
+            #           - we check again, because we may need to turn twice
+            #       - if the next square is ., we move forwards
+            #       - if the next square is out of bounds, there's no loop
+            #       - if the next square is our start position, there's a loop
+#   - move to the next square
+
+def check_next_square(pos, direction, map_v):
+    """Takes a position and direction, and returns the contents of the next square"""
+    new_pos = [pos[0] + direction[0], pos[1] + direction[1]]
+    if (   new_pos[0] < 0
+        or new_pos[1] < 0
+        or new_pos[0] >= len(map_v)
+        or new_pos[1] >= len(map_v[0])
+    ):
+        return 'Z' # we'll use this for out of bounds
+    elif map_v[new_pos[0]][new_pos[1]] == '#':
+        return '#'
+    elif map_v[new_pos[0]][new_pos[1]] == '.':
+        return '.'
+    elif map_v[new_pos[0]][new_pos[1]] == '^':
+        [print(''.join(line)) for line in map_v]
+        quit()
+
+def turn(direction):
+    """Takes a direction, and turns it to the right"""
+    new_direction = [direction[1], -direction[0]]
+    return new_direction
+
+def move_forward(pos, direction, map_v):
+    map_v[pos[0]][pos[1]] = '.'
+    map_v[pos[0] + direction[0]][pos[1] + direction[1]] = '^'
+    new_pos = [pos[0] + direction[0],pos[1] + direction[1]]
+    return new_pos
+
+def check_for_loop(start_pos, direction):
+    map_copy = map_grid.copy()
+    map_copy[start_pos[0] + direction[0]][start_pos[1] + direction[1]] = '#'
+    check_over = False
+    current_pos = start_pos
+    visited_vectors = set()
+    while not check_over:
+        next_square = check_next_square(current_pos, direction, map_copy)
+        current_vector = tuple(current_pos + direction)
+        if current_vector in visited_vectors:
+            map_copy[current_pos[0]][current_pos[1]] = '.'
+            return True
+        elif next_square == '#':
+            direction = turn(direction)
+            continue
+        elif next_square == '.':
+            visited_vectors.add(current_vector)
+            current_pos = move_forward(current_pos, direction, map_copy)
+            continue
+        elif next_square == 'Z':
+            map_copy[current_pos[0]][current_pos[1]] = '.'
+            return False
+            
 map_grid = [list(row) for row in data]
 game_over = False
+loop_count = 0
+current_pos = [[map_grid.index(row), row.index(char)] for row in map_grid for char in row if char == '^'][0]
 direction = [-1, 0]
-opportunities = 0
-guard_position = [[map_grid.index(row), row.index(char)] for row in map_grid for char in row if char == '^'][0]
-
-def see_loop(position, direction, original_position, times=1):
-    if times > 900:
-        return False
-    loop = False
-    # first we turn our head to the right
-    direction = [direction[1], -direction[0]]
-    # then we scan ahead, looking for the next non '.'
-    still_looking = True
-    while still_looking:
-        new_position = [position[0] + direction[0], position[1] + direction[1]]
-        # checking bounds
-        if (   new_position[0] < 0
-            or new_position[1] < 0
-            or new_position[0] >= len(map_grid)
-            or new_position[1] >= len(map_grid[0])
-        ):
-            still_looking = False
-        else:
-            if new_position == original_position:
-                loop = True
-                still_looking = False
-            elif map_grid[new_position[0]][new_position[1]] == '+':
-                loop = True
-                still_looking = False
-            elif map_grid[new_position[0]][new_position[1]] == '^':
-                loop = True
-                still_looking = False
-            elif (map_grid[new_position[0]][new_position[1]] == '#'
-                and map_grid[position[0]][position[1]] == 'X'):
-                loop = True
-                still_looking = False
-            elif map_grid[new_position[0]][new_position[1]] == '#':
-                loop = see_loop(position, direction, original_position, times+1)
-                still_looking = False
-        position = new_position
-
-    return loop
 
 while not game_over:
-    new_direction = get_direction(guard_position, direction)
-    turned = True if new_direction != direction else False
-    direction = new_direction
-    new_position = [guard_position[0] + direction[0], guard_position[1] + direction[1]]
-    # first we'll check if the new position is off the map
-    if (   new_position[0] < 0
-        or new_position[1] < 0
-        or new_position[0] >= len(map_grid)
-        or new_position[1] >= len(map_grid[0])
-    ):
-        game_over = True
-    elif turned:
-        # don't check for loop
+    next_square = check_next_square(current_pos, direction, map_grid)
+    if next_square == '#':
+        direction = turn(direction)
         continue
-    else:
-        # first we'll check if there's a loop made from turning right
-        opportunities += 1 if see_loop(new_position, direction, new_position) else 0
-        # next, we'll paint the map, painting + if we turned here
-        map_grid[new_position[0]][new_position[1]] = '^'
-        map_grid[guard_position[0]][guard_position[1]] = '+' if turned else 'X'
+    elif next_square == '.':
+        if check_for_loop(current_pos, direction):
+            loop_count += 1
+        current_pos = move_forward(current_pos, direction, map_grid)
+    elif next_square == 'Z':
+        game_over = True
 
-        guard_position = new_position
-
-# [print(''.join(row)) for row in map_grid]
-
-print('The number of obstruction possibilities amount to', opportunities)
+print('loop count', loop_count)
